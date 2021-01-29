2dcd54accc7a67331ab36169f95e9165f98ad2a3
Nathan Griffiths <ngriffiths@postmedia.com>
2021-01-29 12:22:08 -0800

    handles no deaths better in text

--- src/js/components/TooltipTemplate/tooltip-template.js
+++ src/js/components/TooltipTemplate/tooltip-template.js
@@ -3,11 +3,19 @@ import helper from '../../../js/helper-functions';
 
 
 function tooltip(data) {
+	let death_graf;
+
+	if (data.latest_deaths > 0) {
+		death_graf = `<p>At least <span class="black">${helper.numberWithCommas(data.latest_deaths)} people have died</span> in ${data.name} from the virus.</p>`
+	} else {
+		death_graf = `<p>There haven’t been any death reported from the virus</span> in ${data.name}.</p>`
+	}
+
 	const template = `
 		<div class="tooltip-content">
 			<h4>${data.name}</h4>
 			<p>There are currently <span class="blue">${helper.numberWithCommas(data.latest_active)} active cases</span> in ${data.name} – roughly ${data.latest_active_100k} cases per 100,000 people.</p>
-			<p>At least <span class="black">${helper.numberWithCommas(data.latest_deaths)} people have died</span> in ${data.name} from the virus.</p>
+			${death_graf}
 		</div>
 	`;
 
